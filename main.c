56e6e7a39f826ed97d9deded8efe16a5d61b6497
tushevg <georgi.tushev@brain.mpg.de>
2020-03-28 12:33:17 +0100

    test bitseq

--- main.c
+++ main.c
@@ -4,33 +4,25 @@
 #include "tests.h"
 
 
-
-
-
-
-
-
 int main(int argc, const char *argv[])
 {
     aux_config_t aux;
     htsu_config_t hts;
 
-
-    //if (run_tests() > 0)
-    //    return 0;
+    if (run_tests() > 0)
+        return 0;
 
     // parse auxiliary configuration
     if (aux_parse(&aux, argc, argv) != 0)
         return 1;
 
     // open FASTQ files
-    /*
     if (htsu_open(&hts, aux.fh_input, aux.fh_pair) != 0)
         return aux_help();
 
     htsu_parse(&hts, aux.qual);
     //
     htsu_destroy(&hts);
-    */
+
     return 0;
 }
--- tests.c
+++ tests.c
@@ -1,29 +1,38 @@
 #include "tests.h"
 
-int run_tests(void) {
+int run_tests(void)
+{
     int result = 0;
-
     result += test_bs_packseq();
     result += test_bs_bitset();
-
     return result;
 }
 
 
-int test_bs_packseq(void) {
+int test_bs_packseq(void)
+{
     int result = 0;
     const char *dna = "ACGT";
     bitseq_t *bs = bs_pack(dna);
     result = (bs->bitseq[0] == 30) ? 1 : 0;
     bs_print(bs);
+    printf("bit%d = %d\n", 3, bs_exist(bs, 3));
+
+    bs_set(bs, 3, 0);
+    bs_print(bs);
+    printf("bit%d = %d\n", 3, bs_exist(bs, 3));
+
+    bs_set(bs, 3, 1);
+    bs_print(bs);
+    printf("bit%d = %d\n", 3, bs_exist(bs, 3));
+
     bs_destroy(bs);
     return result;
 }
 
 
-int test_bs_bitset(void) {
+int test_bs_bitset(void)
+{
     int result = 0;
-
-
     return result;
 }
